83c6cac1f30485849983a39d8daf4af146cb7938
Minh To <to.nguyet.minh@gmail.com>
2017-07-18 16:45:16 -0700

    debugging

--- sendReminders.js
+++ sendReminders.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var RtmClient = require('@slack/client').RtmClient;
 var bot_token = process.env.SLACK_BOT_TOKEN || '';
 var rtm = new RtmClient(bot_token);
-
+rtm.sendMessage('inside script', 'D6AS82SQ6');
 var today = new Date().toISOString().substring(0, 10); // change today's date to 'yyyy-mm-dd' format
 var tomorrow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toISOString().substring(0, 10); // change tomorrow's date to 'yyyy-mm-dd' format
 Reminder.find({date: {$in: [today, tomorrow]}}, function(err, reminders) { // find all reminders due today or tomorrow
@@ -17,7 +17,7 @@ Reminder.find({date: {$in: [today, tomorrow]}}, function(err, reminders) { // fi
     reminders.forEach((reminder) => {
       console.log('rtm.getDMByUserId:', rtm.dataStore.getDMByUserId(reminder.userId));
       var dm = rtm.dataStore.getDMByUserId(reminder.userId); // dm object by userId
-      var channel = rtm.dataStore.getDMByUserId(reminder.userId).id; // dm channel by userId
+      var channel = dm.id; // dm channel by userId
       var date = (reminder.date === today? "today" : "tomorrow") // converts date to string 'today' or 'tomorrow'
       rtm.sendMessage('Reminder: You need to ' + reminder.subject + ' ' + date, channel) // send message to DM
     })
