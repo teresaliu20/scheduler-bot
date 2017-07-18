7539e9a96fc09189728647f5fbaffb653d938f1d
Minh To <to.nguyet.minh@gmail.com>
2017-07-18 14:13:00 -0700

    random changes

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 var google = require('googleapis');
-
 var OAuth2 = google.auth.OAuth2;
 var oauth2Client = new OAuth2(
     process.env.GOOGLE_CLIENT_ID,
@@ -18,15 +17,12 @@ var oauth2Client = new OAuth2(
     process.env.DOMAIN + '/connect/callback'
 );
 
-var calendar = google.calendar('v3');
-
 var models = require('./models');
 var User = models.User;
 var Reminder = models.Reminder;
 
 // Redirects to Google OAuth2
 app.get('/google/oauth', function(req, res) {
-    // Generate a redirect url when user logs into google
     var url = oauth2Client.generateAuthUrl({
         access_type: 'offline',
         prompt: 'consent',
@@ -34,34 +30,29 @@ app.get('/google/oauth', function(req, res) {
             'https://www.googleapis.com/auth/userinfo.profile',
             'https://www.googleapis.com/auth/calendar'
         ],
-        // Encodes the auth_id from the passed in query that saves current user's id
         state: encodeURIComponent(JSON.stringify({
             auth_id: req.query.auth_id
         }))
     })
-    // Redirect to the generated URL
     res.redirect(url);
 });
 
-// Handles callback created from Google OAuth2 when the user logs in
+// Handles callback from Google OAuth2
 app.get('/connect/callback', function(req, res) {
-    // Decodes the auth_id from query, used to find current user from database
     var authId = JSON.parse(decodeURIComponent(req.query.state)).auth_id;
-    // Authorization code generated from the google server using the request token
     var code = req.query.code;
     console.log("AUTH ID : ", authId);
     oauth2Client.getToken(code, function (err, tokens) {
-        // Now tokens contains an access_token and an optional refresh_token that we save
+        // Now tokens contains an access_token and an optional refresh_token. Save them.
         if (!err) {
             oauth2Client.setCredentials(tokens);
             console.log("TOKENS: ",tokens);
-            // Save the tokens to the current user in the database to use later
             User.findByIdAndUpdate(authId, { $set: {google: tokens}}, function(err, user) {
                 if (err) {
                     res.send({success: false, error: err});
                 }
                 else {
-                    console.log('FOUND USER', user);
+                    console.log(user);
                     res.send({success: true});
                 }
             })
@@ -74,84 +65,34 @@ app.get('/connect/callback', function(req, res) {
 
 })
 
+
 // Route to handle interactive message actions
 app.post('/slack/interactive', function(req, res) {
-    console.log("OAUTH2CLIENT", oauth2Client);
-
-
-
-    // Payload contains the interactive message information and event
     var payload = JSON.parse(req.body.payload);
-    console.log('PAYLOAD INTERACTIVE', payload);
-
-    // Must retrieve token associated with user stored in database
-    User.findOne({slackId: payload.user.id}, function(err, user) {
-        if (err) {
-            console.log("ERROR FINDING USER", err);
-        }
-        else {
-            oauth2Client.setCredentials({
-                'access_token': user.google.access_token,
-                'refresh_token': user.google.refresh_token
-            });
-            // If the user clicked confirm, create Google Calendar event
-            if (payload.actions[0].value === 'confirm') {
-                var attachment = payload.original_message.attachments[0]; // make a copy of attachments (the interactive part)
-                delete attachment.actions; // delete buttons
-                attachment.text = 'Reminder set'; // change the text after the the confirm button was clicked
-                attachment.color = '#53B987' // change the color to green
-                res.json({
-                    replace_original: true, // replace the original interactive message box with a new messagee
-                    text: 'Created reminder :white_check_mark:',
-                    attachments: [attachment]
-                });
-                // Retrieving the subject of the event in attachment fallback
-                var subject = payload.original_message.attachments[0].fallback;
-                // Retrieving the date of the event in attachment pretext
-                var date = payload.original_message.attachments[0].pretext;
-                // Create the event for the Google Calendar API
-                let reminderEvent = {
-                    'summary': subject,
-                    'location': '',
-                    'description': '',
-                    'start': {
-                        'date': date
-                    },
-                    'end': {
-                        'date': date
-                    }
-                }
-                // Insert the event into the user's primary calendar
-                calendar.events.insert({
-                    auth: oauth2Client,
-                    'calendarId': 'primary',
-                    'resource': reminderEvent
-                }, function(err, resp) {
-                    if (err) {
-                        console.log("ERROR INSERTING INTO GOOGLE CALENDAR: ", err);
-                    }
-                    else {
-                        console.log("REMINDER INSERTED INTO GOOGLE CALENDAR");
-                    }
-                })
-            }
-            else {
-                // If the cancel button is pressed instead, cancel the event
-                var attachment = payload.original_message.attachments[0];
-                delete attachment.actions;
-                attachment.fallback = '';
-                attachment.pretext = '';
-                console.log(attachment);
-                attachment.text = 'Cancelled reminder';
-                attachment.color = '#DD4814'
-                res.json({
-                    replace_original: true,
-                    text: 'Cancelled reminder :x:',
-                    attachments: [attachment]
-                });
-            }
-        }
-    })
+    console.log('BODY', payload);
+    if (payload.actions[0].value === 'confirm') {
+        var attachment = payload.original_message.attachments[0]; // make a copy of attachments (the interactive part)
+        delete attachment.actions; // delete buttons
+        attachment.text = 'Reminder set'; // change the text after the the confirm button was clicked
+        attachment.color = '#53B987' // change the color to green
+        res.json({
+            replace_original: true, // replace the original interactive message box with a new messagee
+            text: 'Created reminder :white_check_mark:',
+            attachments: [attachment]
+        });
+    }
+    else {
+        var attachment = payload.original_message.attachments[0];
+        delete attachment.actions;
+        console.log(attachment);
+        attachment.text = 'Cancelled reminder';
+        attachment.color = '#DD4814'
+        res.json({
+            replace_original: true,
+            text: 'Cancelled reminder :x:',
+            attachments: [attachment]
+        });
+    }
 });
 
 app.listen(3000, function() {
