9fd953fc70ccb2d87fb3634ef63da28a9c217b3a
oadeboye <oadeboye@wellesley.edu>
2017-07-20 10:32:39 -0700

    Decided to make new branch to fix conflicts to search using CalendarList.list and freebusy google apis

--- bothelp.js
+++ bothelp.js
@@ -20,41 +20,40 @@ var reminderResponse = function(parameters) {
   return words;
 }
 
-var meetingResponse = function(parameters) {
-  var dateStr = (new Date(parameters.date)).toDateString();
-  var words = 'Schedule a meeting with ' + parameters.invitees + ' on ' + parameters.date + ' at ' + parameters.time;
-  return words;
-}
+// var meetingResponse = function(parameters) {
+//   var dateStr = (new Date(parameters.date)).toDateString();
+//   var words = 'Schedule a meeting with ' + parameters.invitees + ' on ' + parameters.date + ' at ' + parameters.time;
+//   return words;
+// }
 
-var messageObject = {
-  "text": "Would you like to play a game?",
-  "attachments": [
-    {
-      "text": "Would you like to confirm?",
-      "fallback": "You are unable to choose an action",
-      "callback_id": "action",
-      "color": "#3AA3E3",
-      "attachment_type": "default",
-      "actions": [
-        {
-          "name": "action",
-          "text": "Confirm",
-          "type": "button",
-          "value": "confirm"
-        },
-        {
-          "name": "action",
-          "text": "Cancel",
-          "type": "button",
-          "value": "cancel"
-        }
-      ]
-    }
-  ]
-}
+// var messageObject = {
+//   "text": "Would you like to play a game?",
+//   "attachments": [
+//     {
+//       "text": "Would you like to confirm?",
+//       "fallback": "You are unable to choose an action",
+//       "callback_id": "action",
+//       "color": "#3AA3E3",
+//       "attachment_type": "default",
+//       "actions": [
+//         {
+//           "name": "action",
+//           "text": "Confirm",
+//           "type": "button",
+//           "value": "confirm"
+//         },
+//         {
+//           "name": "action",
+//           "text": "Cancel",
+//           "type": "button",
+//           "value": "cancel"
+//         }
+//       ]
+//     }
+//   ]
+// }
 
-function meetOrRemind(data, message, user){
-  console.log("DATA: ",data);
+function meetOrRemind(data, message, user, fullName){
   if (data.result.actionIncomplete) {
     rtm.sendMessage(data.result.fulfillment.speech, message.channel);
   }
@@ -75,29 +74,8 @@ function meetOrRemind(data, message, user){
     })
   }
   else if (data.result.action === 'meeting:add') {
-    console.log("MESSAGE MEETING: ", data.result.parameters);
     //before sending the confirmation, check to see that the meeting slot is free for all invitees
-    resolveConflicts(data.result.parameters, user);
-
-    //then, send confirmation message
-    web.chat.postMessage(message.channel, meetingResponse(data.result.parameters), messageObject);
-    user.pendingState = JSON.stringify({
-      type: 'meeting',
-      date: data.result.parameters.date,
-      startTime: data.result.parameters.time[0], // startTime in time array
-      endTime: data.result.parameters.time[1] || '', // endTime in time array
-      invitees: data.result.parameters.invitees || '',
-      description: data.result.parameters.subject || '',
-      location: data.result.parameters.location || ''
-    });
-    user.save(function(err, found){
-    console.log(found);
-    if (err){
-      console.log('error finding user with id', user._id);
-      } else {
-      console.log('user found and pending state set! yay.');
-      }
-    })
+    resolveConflicts(data.result.parameters, user, message);
   }
   else if (!data.result.actionIncomplete){
     rtm.sendMessage(data.result.fulfillment.speech, message.channel);
--- conflicts.js
+++ conflicts.js
@@ -3,6 +3,9 @@
 var models = require('./models');
 var Meeting = models.Meeting;
 
+//require underscore
+var _ = require('underscore');
+
 //QUESTION: Should I be searching with data.result.parameters or with pendingState?
 
 // set an empty array to catch all conflicts, and another for resolutions
@@ -10,36 +13,35 @@ var conflict = [];
 var resolutions = [];
 
 // get the planned meeting duration
-var duration = ( pendingState.endTime - pendingState.startTime )|| 60*60*1000
-
-// parse pendingState invitees, which is an array of slackIds
-var pendingState = JSON.stringify(user.pendingState)
-var invitees = pendingState.invitees;
+var duration = ( pendingState.endTime - pendingState.startTime )|| 60*60*1000;
 
 // Search all meetings by invitees
 // Loop through invitees
-invitees.forEach(function(invitee){
-  Meeting.find({userId: invitee}, function (err, found){
-    if (err){
-      console.log("Error finding user", err);
-      return;
-    } else if (!found){
-      console.log("No such meetings found, you can set a meeting!");
-      //send the user an interactive message so they can confirm or cancel the meeting
-      return;
-    } else {
-      // check that the times for this user's other meetings do not conflict
-      var foundStart = (new Date(found.startTime)).toString();
-      var foundEnd = (new Date(found.endTime)).toString();
-      var pendingStart = (new Date(pendingState.startTime)).toString();
-      if (found.date === pendingState.date && foundStart <= pendingState.startTime && foundEnd >= pendingStart ){
-        console.log("This is a time conflict with ", found.userId);
-        //store the meeting in an array of time conflicts
-        conflict.push(found);
+function findThatConflict(inviteeArray){
+  inviteeArray.forEach(function(invitee){
+    Meeting.find({userId: invitee}, function (err, found){
+      if (err){
+        console.log("Error finding user", err);
+        return;
+      } else if (!found){
+        console.log("No such meetings found, you can set a meeting!");
+        //send the user an interactive message so they can confirm or cancel the meeting
+        web.chat.postMessage(message.channel, meetingResponse(parameters), messageObject);
+        return;
+      } else {
+        // check that the times for this user's other meetings do not conflict
+        var foundStart = (new Date(found.startTime)).toString();
+        var foundEnd = (new Date(found.endTime)).toString();
+        var pendingStart = (new Date(pendingState.startTime)).toString();
+        if (found.date === pendingState.date && foundStart <= pendingState.startTime && foundEnd >= pendingStart ){
+          console.log("This is a time conflict with ", found.userId);
+          //store the meeting in an array of time conflicts
+          conflict.push(found);
+        }
       }
-    }
-  })
-});
+    })
+  });
+}
 
 // Convert start and end times for each conflict to milliseconds
 var conflictFix = conflict.map(function(meeting){
@@ -50,7 +52,9 @@ var conflictFix = conflict.map(function(meeting){
 // if new meeting start time falls within existing meeting times,
 // find different times available for invitees
 conflictFix.forEach(function(conflict){
-    for (var i = 0; i < 10; i++) {
+  for (var j = 0; j < 3; j++) {
+
+    for (var i = 0; i < 3; i++) {
       var newTimeSlot = {
         "text": (new Date(conflict.end + (duration * i))).toDateString(),
         "value": JSON.stringify({
@@ -61,6 +65,7 @@ conflictFix.forEach(function(conflict){
       };
       resolutions.push(newTimeSlot);
     }
+  }
 })
 
 // send an interactive message with different times available to user
@@ -79,20 +84,87 @@ var responseObj = {
                     "name": "times_list",
                     "text": "Pick a time...",
                     "type": "select",
-                    "options": resolutions.slice(0,10)
+                    "options": resolutions.slice(0, 10)
                 }
             ]
         }
     ]
 }
 
-web.chat.postMessage(message.channel, meetingResponse(data.result.parameters), responseObject);
+var messageObject = {
+  "text": "Would you like to play a game?",
+  "attachments": [
+    {
+      "text": "Would you like to confirm?",
+      "fallback": "You are unable to choose an action",
+      "callback_id": "action",
+      "color": "#3AA3E3",
+      "attachment_type": "default",
+      "actions": [
+        {
+          "name": "action",
+          "text": "Confirm",
+          "type": "button",
+          "value": "confirm"
+        },
+        {
+          "name": "action",
+          "text": "Cancel",
+          "type": "button",
+          "value": "cancel"
+        }
+      ]
+    }
+  ]
+}
+
+var meetingResponse = function(parameters) {
+  var dateStr = (new Date(parameters.date)).toDateString();
+  var words = 'Schedule a meeting with ' + parameters.invitees + ' on ' + parameters.date + ' at ' + parameters.time;
+  return words;
+}
+
 
 // when user clicks, make new meeting with the time from that timeslot
 
-function resolveConflicts(parameters, user){
+function resolveConflicts(parameters, user, message){
+  // parse pendingState invitees, which is an array of slackIds
+  var pendingState = JSON.stringify(user.pendingState);
+  var invitees = pendingState.invitees;
+  //transform invitee array with fullNames to an array of their slackIds from the database
+  invitees.map(function(invitee){
+    User.findOne({fullName: invitee}, function(err, found){
+      if (found){
+        return found.slackId;
+      } else if (err){
+        console.log("Error with database");
+      } else {
+        console.log("Error finding invitee");
+      }
+    })
+  })
+  findThatConflict(invitees);
+  web.chat.postMessage(message.channel, meetingResponse(parameters), responseObject);
 
+  //then, reset pending State
+  user.pendingState = JSON.stringify({
+    type: 'meeting',
+    date: parameters.date,
+    startTime: parameters.time[0], // startTime in time array
+    endTime: parameters.time[1] || '', // endTime in time array
+    invitees: parameters.invitees || '',
+    description: parameters.subject || '',
+    location: parameters.location || ''
+  });
 
+  user.save(function(err, found){
+  console.log(found);
+  if (err){
+    console.log('error finding user with id', user._id);
+    } else {
+    console.log('user found and pending state set! yay.');
+    }
+  });
 
 };
 
